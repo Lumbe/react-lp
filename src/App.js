352889a648e528fb730068544c7a2dc324c1fa99
Lumbe <just.me.sober@gmail.com>
2017-09-19 16:33:02 +0300

    bootstrap tweak

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-// import './css/customBootstrap.css'
-import 'bootstrap/dist/css/bootstrap.css';
+import './css/customBootstrap.css'
+// import 'bootstrap/dist/css/bootstrap.css';
 import Routes from './routes'
 
 class App extends Component {
