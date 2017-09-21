35f770895be94496f99282061d8e3e05ae50d78d
Lumbe <just.me.sober@gmail.com>
2017-09-21 18:09:11 +0300

    project variables & Museo and Proxima fonts

--- src/components/secondPage/secondPage.jsx
+++ src/components/secondPage/secondPage.jsx
@@ -4,8 +4,21 @@ import React from 'react'
 class SecondPage extends React.Component {
 
   render() {
-    return (
-      <p>Second Page!</p>
+    return (<div>
+      <h1>H1 Proxima Nova шрифт</h1>
+      <h2>H2 header</h2>
+      <h3>H3 header</h3>
+      <h4>p header</h4>
+      <h5>H5 header</h5>
+      <h6>H6 header</h6>
+      <h1>Museo шрифт</h1>
+      <p>Second Page Paragraph!</p>
+      <a>Link</a>
+      <p style={{fontWeight: 300}}>Нрмальный текст</p>
+      <p style={{fontWeight: 500}}>Жирный текст</p>
+      <p style={{fontWeight: 700}}>Совсем жирный текст</p>
+      <p style={{fontWeight: 900}}>Омг какой жирный текст</p>
+    </div>
     )
   }
 }
