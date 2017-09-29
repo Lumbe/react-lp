f15a607afc0bb8d556dbc4fb2ec5978c204a524b
Lumbe <just.me.sober@gmail.com>
2017-09-29 12:04:19 +0300

    notFound page & vertical menu fix

--- src/components/error/notFound.jsx
+++ src/components/error/notFound.jsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import {setWindowHeight,resetWindowHeight} from "../common/main";
 
-export default function NotFound(props) {
-  return <p>Page not found!</p>
+export default class NotFound extends React.Component {
+  componentWillMount() {
+    setWindowHeight();
+    window.onresize = setWindowHeight;
+  }
+
+  componentWillUnmount() {
+    resetWindowHeight();
+    window.onresize = null;
+  }
+
+  render() {
+    return <p>Страница не найдена</p>
+  }
 }
