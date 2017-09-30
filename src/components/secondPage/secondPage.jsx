a3be9f3fce1b9edc18f1bb2d3bfa6cf4d4f034d9
Lumbe <just.me.sober@gmail.com>
2017-09-30 18:50:07 +0300

    second page

--- src/components/secondPage/secondPage.jsx
+++ src/components/secondPage/secondPage.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import './secondPage.css'
 import {setWindowHeight,resetWindowHeight, setBackgroundImage, removeBackgroundImage} from "../common/main";
+import {Row, Col, Image, Modal} from 'react-bootstrap'
 import backgroundImage from './bg-screen2.jpg'
+import videoImage from './video.jpg'
+import YouTube from 'react-youtube'
+import FontAwesome from 'react-fontawesome'
 
 class SecondPage extends React.Component {
   constructor(props) {
@@ -10,8 +15,17 @@ class SecondPage extends React.Component {
 
   defaultProps() {
     return {
-      // backgroundUrl: "/images/bg-screen.jpg"
+      showModal: false
     }
+  }
+
+  open() {
+    this.setState({showModal: true})
+  }
+
+  close() {
+    this.setState({showModal: false})
+
   }
 
   componentWillMount() {
@@ -20,6 +34,7 @@ class SecondPage extends React.Component {
     setBackgroundImage(backgroundImage);
   }
 
+
   componentWillUnmount() {
     resetWindowHeight();
     removeBackgroundImage();
@@ -27,8 +42,69 @@ class SecondPage extends React.Component {
   }
 
   render() {
-    return (<div>
-
+    const opts = {
+      width: '100%',
+      playerVars: {
+        autoplay: 1
+      }
+    };
+    return (<div className="second-section">
+      <Row>
+        <Col md={4}>
+          <div className="yt-cover"  onClick={this.open.bind(this)}>
+            <div className="cover-frame"/>
+            <Image src={videoImage} responsive/>
+            <FontAwesome className="play-icon" name="play-circle" size="4x"/>
+          </div>
+        </Col>
+        <Col md={8}>
+          <div className="company-info"><h1>Производственно строительная компания <span className="text-highlight">Сервус</span></h1>
+          <h4>Национальный производитель энергоэффективных домов</h4>
+          <p className="p-highlight">Строим энергосберегающие дома и коттеджи из СИП-панелей в Виннице и Винницкой области.
+            Компания Сервус первая в Украине представила канадскую технологию строительства и адаптировала
+            для строительства в нашей климатической зоне.<br/> Мы осуществляем полный цикл строительных работ -
+            от разработки проекта дома до сдачи объекта "под ключ". Проектируем, производим и строим в самые короткие сроки -
+            дом "под ключ" на 120кв.м. строится за 3 месяца. </p></div>
+        </Col>
+      </Row>
+        <Row className="advantages">
+          <Col md={2} mdOffset={1}>
+            <div className="adv-block">
+              <span className="adv-number">12</span> <span className="adv-text">лет</span>
+            </div>
+            <p>на рынке энергосберегающего жилья</p>
+          </Col>
+          <Col md={2}>
+            <div className="adv-block">
+              <span className="adv-number">1248</span>
+            </div>
+            <p>энергоэффективных зданий построено в Украине и ЕС</p>
+          </Col>
+          <Col md={2}>
+            <div className="adv-block">
+              <span className="adv-number">150</span> <span className="adv-text">тыс</span>
+            </div>
+            <p>кв.м. недвижимости введено в эксплуатацию</p>
+          </Col>
+          <Col md={2}>
+            <div className="adv-block">
+              <span className="adv-number">10</span> <span className="adv-text">лет</span>
+            </div>
+            <p>гарантийного обслуживания</p>
+          </Col>
+          <Col md={2}>
+            <div className="adv-block">
+              <span className="adv-number">300</span>
+            </div>
+            <p>работников компании трудятся для вас</p>
+          </Col>
+        </Row>
+        <Modal dialogClassName="yt-modal" keyboard={this.onHide} show={this.state.showModal} onHide={this.close.bind(this)}>
+            <YouTube
+              videoId="SDfBkztAchs"
+              opts={opts}
+            />
+        </Modal>
     </div>
     )
   }
