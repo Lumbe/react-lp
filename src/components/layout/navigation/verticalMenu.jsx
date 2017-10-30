45fd664865edb820a7d1dc27ff7eb534ed325166
Lumbe <just.me.sober@gmail.com>
2017-10-30 14:37:41 +0200

    refactoring & deploy scripts

--- src/components/layout/navigation/verticalMenu.jsx
+++ src/components/layout/navigation/verticalMenu.jsx
@@ -2,37 +2,17 @@ import React from 'react'
 import './verticalMenu.css'
 import {Nav, NavItem} from 'react-bootstrap'
 import {IndexLinkContainer, LinkContainer} from "react-router-bootstrap";
-import {Popover, OverlayTrigger} from 'react-bootstrap'
 
 class VerticalMenu extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     activeKey: 1
-  //   };
-  // }
-
-  // handleSelect(selectedKey) {
-  //   this.setState({
-  //     activeKey: selectedKey
-  //   });
-  // }
 
   render() {
-    const popoverHoverFocus = (
-        <Popover id="popover-trigger-hover-focus" title="Дом под ключ за 3 месяца">
-          <strong>Энергосберегающий</strong> очень!
-        </Popover>
-    );
     return (
           <Nav className="vertical-menu pull-right  hidden-xs hidden-sm">
-            {/*<OverlayTrigger trigger={['hover', 'focus']} placement="left" overlay={popoverHoverFocus}>*/}
-              <IndexLinkContainer to="/">
-                <NavItem eventKey={1}>
-                  <div className="inner-dot"/>
-                </NavItem>
-              </IndexLinkContainer>
-            {/*</OverlayTrigger>*/}
+            <IndexLinkContainer to="/">
+              <NavItem eventKey={1}>
+                <div className="inner-dot"/>
+              </NavItem>
+            </IndexLinkContainer>
             <LinkContainer to="/about">
               <NavItem eventKey={2}>
                 <div className="inner-dot"/>
@@ -93,7 +73,6 @@ class VerticalMenu extends React.Component {
                 <div className="inner-dot"/>
               </NavItem>
             </LinkContainer>
-
           </Nav>
     )
   }
