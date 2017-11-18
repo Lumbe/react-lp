b6f9549a1446ec46a0067435d3004c772b4a7d25
Lumbe <just.me.sober@gmail.com>
2017-11-18 22:38:59 +0200

    fix

--- src/components/project/projectIndex.jsx
+++ src/components/project/projectIndex.jsx
@@ -54,7 +54,7 @@ class ProjectIndex extends React.Component {
         if (response.data.sent) {
           this.setState({animateIn: false});
           setTimeout(() => {
-            this.finishFormSubmission()
+            this.toggleFormSubmission()
           }, 800);
         }
       },
