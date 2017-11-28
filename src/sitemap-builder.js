c20d922b238d0c139fb95466413de09e5c5ef4ad
Lumbe <just.me.sober@gmail.com>
2017-11-28 10:58:33 +0200

    remove react-router-sitemap

--- src/sitemap-builder.js
+++
@@ -1,9 +0,0 @@
-require('babel-register');
-const router = require('./siteMapRouter').default;
-const Sitemap = require('react-router-sitemap').default;
-
-(
-  new Sitemap(router)
-    .build('https://servus.vn.ua')
-    .save('./sitemap.xml')
-);
