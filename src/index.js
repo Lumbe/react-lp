2d3a794ee56b2db547d6130d25daf0fdcdd72aa9
Lumbe <just.me.sober@gmail.com>
2017-12-01 18:18:19 +0200

    replace react-snapshot with react-snap

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
-import { render } from 'react-snapshot'
 import registerServiceWorker from './registerServiceWorker';
 
-render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
