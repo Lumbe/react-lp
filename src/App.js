16514cc4299d3749ece593312f2c0cbe4ef8bc55
Lumbe <just.me.sober@gmail.com>
2017-12-03 20:33:51 +0200

    css dynamic import example

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Routes from './routes'
 import ReactGA from 'react-ga';
 ReactGA.initialize('UA-109956063-1');
 
+// (() => import('./css/main.css'))();
+
 class App extends Component {
   render() {
     return (
