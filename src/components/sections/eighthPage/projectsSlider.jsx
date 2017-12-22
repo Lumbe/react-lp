3cfb726bf07b47992ce44cc124ff2333a04faad1
Lumbe <just.me.sober@gmail.com>
2017-12-22 13:06:10 +0200

    popular projects page

--- src/components/sections/eighthPage/projectsSlider.jsx
+++ src/components/sections/eighthPage/projectsSlider.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import OwlCarousel from 'react-owl-carousel'
 import './owl.theme.projects.css'
 import ProjectDescription from './projectDescription'
-import {Button} from 'react-bootstrap'
 
 class ProjectsSlider extends React.Component {
   constructor(props) {
@@ -16,12 +15,9 @@ class ProjectsSlider extends React.Component {
     const iconLeft = "<span class='fa fa-angle-left fa-2x'/>";
     const iconRight = "<span class='fa fa-angle-right fa-2x'/>";
     let projects = this.props.projects || this.state.projects;
-    const footer = <Button bsStyle="link" className="btn-yellow">
-      Смотреть планировку
-    </Button>;
     let items =[];
     projects.map((project, index) => {
-      return items.push(<ProjectDescription key={index} footer={footer} project={project}/>);
+      return items.push(<ProjectDescription key={index} project={project}/>);
     });
     return (
       <OwlCarousel
